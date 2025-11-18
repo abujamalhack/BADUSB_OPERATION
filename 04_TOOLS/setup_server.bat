676e02bfce2b97245856d124832cbef6ef158810
𝐴𝑏𝑢 𝐽𝑎𝑚𝑎𝑙 | CyberPhantom <mccarthy29mp@gmail.com>
2025-11-18 18:54:38 +0300

    Create setup_server.bat

---
+++ 04_TOOLS/setup_server.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Server Setup Guide
+echo.
+
+echo 1. Create account on Heroku: https://heroku.com
+echo 2. Install Heroku CLI and Git
+echo 3. Create new app on Heroku
+echo 4. Upload all files from 03_SERVER_FILES folder
+echo 5. Don't forget to upload:
+echo    - update_service.apk (the built Android app)
+echo    - platform-tools.zip (download from Android SDK)
+echo.
+
+echo Remember to change "your-actual-server.herokuapp.com" to your actual server URL!
+echo.
+
+pause
